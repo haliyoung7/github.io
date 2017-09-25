4374be54dda1b306d809a387383480b018fbcde7
haliyoung7 <30787197+haliyoung7@users.noreply.github.com>
2017-09-25 17:27:27 -0600

    Add files via upload

---
+++ Homework 2.js
@@ -0,0 +1,10 @@
+var dogBreeds = ["golden retriever", "pitbull", "american bulldog", "st. bernard", "weimeraner"]
+
+var myBreed = "golden retriever"
+var userBreed = prompt("What is your favorite dog breed?");
+
+if (myBreed === userBreed) {
+    console.log("We're besties!");
+} else {
+    console.log("You're wrong!");
+}
